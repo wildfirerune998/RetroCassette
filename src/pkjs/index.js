be855bdb6aacd92d7ef29a13ba7a1e9d6ef87816
AdhesiveAntelope <helena.blanchard998@gmail.com>
2025-03-26 20:50:03 -0400

    Update to adjust capstans during the day
    - Also fixed a few bugs and visibility issues

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -6,29 +6,22 @@ var clayConfig = require('./config.json');
 var clay = new Clay(clayConfig);
 
 var era = "";
-
-var xhrRequest = function (url, type, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    callback(this.responseText);
-  };
-  xhr.open(type, url);
-  xhr.send();
-};
+var show_weekday_style = "";
 
 function sendToPebble() {
          
     // Assemble dictionary using our keys
     var dictionary = {
-    "ERA": era
+    "ERA": era,
+    "SHOWWEEKDAYSTYLE": show_weekday_style
     };
 
     // Send to Pebble
     Pebble.sendAppMessage(dictionary, function(e) {
-    //console.log('era info sent to Pebble successfully!');
+    console.log('info sent to Pebble successfully!');
     },
     function(e) {
-        console.log('Error sending era info to Pebble!');
+        console.log('Error sending info to Pebble!');
     });//sendAppMessge
 };
 
@@ -45,7 +38,7 @@ Pebble.addEventListener('ready', function(e) {
         console.log('READY');
       },
       function(e) {
-        console.log('OH NO');
+        console.log('OH FALSE');
       }
      
     );  }
@@ -53,17 +46,27 @@ Pebble.addEventListener('ready', function(e) {
 // Listen for when an AppMessage is received
 Pebble.addEventListener('appmessage', function(e) {
 
-    if (!e.payload){
+    if (!e.payload || typeof e.payload === undefined){
         return;
     }
 
-    //console.log('e.response ' + e.payload);
-    var era_string;
+    console.log('addEventListener appmessage e.response ' + JSON.stringify(e.payload));
+    var era_string = "";
+    var show_weekday_style_string ="";
 
     era_string = JSON.stringify(e.payload.ERA);
+    console.log('appmessage era_string ' + era_string);
     if (era_string) {
         era = era_string;
     }
+
+    show_weekday_style_string = JSON.stringify(e.payload.SHOWWEEKDAYSTYLE);
+    if (!show_weekday_style_string){
+
+    }
+    console.log('appmessage show_weekday_style_string ' + show_weekday_style_string);
+    show_weekday_style = show_weekday_style_string.toString();
+
     sendToPebble();
   }                  
 );
@@ -71,20 +74,26 @@ var messageKeys = require('message_keys');
 
 Pebble.addEventListener('webviewclosed', function(e) {
   
-  //console.log('e.response ' + e.response);
+  console.log('addEventListener webviewclosed e.response ' + e.response);
   if (e && !e.response) {
     return;
   };
   
   // Get the keys and values from each config item
   var claySettings = clay.getSettings(e.response);
-  var era_string;
+  var era_string = "";
+  var show_weekday_style_string ="";
   
   era_string = claySettings[messageKeys.ERA];
+  show_weekday_style_string = claySettings[messageKeys.SHOWWEEKDAYSTYLE];
 
   if (era_string) {
     era = era_string;
   }
+
+  console.log('addEventListener webviewclosed show_weekday_style_string ' + show_weekday_style_string);
+  show_weekday_style = show_weekday_style_string.toString();
+  
   sendToPebble(); 
 
 }
